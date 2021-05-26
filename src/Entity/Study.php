c11940d15f7a159f8bf528bfbd0027677b314d6b
JVincent Garret <vincentgarret33@gmail.com>
2021-05-26 10:43:43 +0200

    creating entity : study

---
+++ src/Entity/Study.php
@@ -0,0 +1,128 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\StudyRepository;
+use Doctrine\ORM\Mapping as ORM;
+use ApiPlatform\Core\Annotation\ApiResource;
+
+/**
+ * @ORM\Entity(repositoryClass=StudyRepository::class)
+ * @ApiResource()
+ */
+class Study
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $ref;
+
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    private $result;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $status;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $date;
+
+    /**
+     * @ORM\Column(type="float")
+     */
+    private $lat;
+
+    /**
+     * @ORM\Column(type="float")
+     */
+    private $lng;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getRef(): ?int
+    {
+        return $this->ref;
+    }
+
+    public function setRef(int $ref): self
+    {
+        $this->ref = $ref;
+
+        return $this;
+    }
+
+    public function getResult(): ?bool
+    {
+        return $this->result;
+    }
+
+    public function setResult(bool $result): self
+    {
+        $this->result = $result;
+
+        return $this;
+    }
+
+    public function getStatus(): ?string
+    {
+        return $this->status;
+    }
+
+    public function setStatus(string $status): self
+    {
+        $this->status = $status;
+
+        return $this;
+    }
+
+    public function getDate(): ?\DateTimeInterface
+    {
+        return $this->date;
+    }
+
+    public function setDate(\DateTimeInterface $date): self
+    {
+        $this->date = $date;
+
+        return $this;
+    }
+
+    public function getLat(): ?float
+    {
+        return $this->lat;
+    }
+
+    public function setLat(float $lat): self
+    {
+        $this->lat = $lat;
+
+        return $this;
+    }
+
+    public function getLng(): ?float
+    {
+        return $this->lng;
+    }
+
+    public function setLng(float $lng): self
+    {
+        $this->lng = $lng;
+
+        return $this;
+    }
+}
